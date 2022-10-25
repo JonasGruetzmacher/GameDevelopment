088a7df801b114d97780cf00a99aac20b8a687e7
OscarEscF <99730368+OscarEscF@users.noreply.github.com>
2022-10-25 12:50:04 +0200

    Update Player.cpp
    jump unfinished

--- citm_desvj_project_template-L08/Game/Source/Player.cpp
+++ citm_desvj_project_template-L08/Game/Source/Player.cpp
@@ -42,28 +42,47 @@ bool Player::Start() {
 	return true;
 }
 
+void isjumping(int jumpspeed) {
+	
+}
+
 bool Player::Update()
 {
 
 	// L07 DONE 5: Add physics to the player - updated player position using physics
 
-	int speed = 10; 
-	b2Vec2 vel = b2Vec2(0, -GRAVITY_Y); 
+	int speed = 10;
+	int jumpspeed = 10;
+
+	b2Vec2 vel = b2Vec2(0, -GRAVITY_Y);
 
 	//L02: DONE 4: modify the position of the player using arrow keys and render the texture
 	if (app->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) {
-		//
+
+		vel = b2Vec2(0, -jumpspeed);
+
+
+
 	}
+
 	if (app->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) {
 		//
 	}
-		
+
 	if (app->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) {
 		vel = b2Vec2(-speed, -GRAVITY_Y);
+		if (app->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) {
+			vel = b2Vec2(-speed, -jumpspeed);
+
+		}
 	}
 
 	if (app->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) {
 		vel = b2Vec2(speed, -GRAVITY_Y);
+		if (app->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) {
+			vel = b2Vec2(speed, -jumpspeed);
+
+		}
 	}
 
 	//Set the velocity of the pbody of the player
@@ -73,7 +92,7 @@ bool Player::Update()
 	position.x = METERS_TO_PIXELS(pbody->body->GetTransform().p.x) - 16;
 	position.y = METERS_TO_PIXELS(pbody->body->GetTransform().p.y) - 16;
 
-	app->render->DrawTexture(texture, position.x , position.y);
+	app->render->DrawTexture(texture, position.x, position.y);
 
 	return true;
 }
